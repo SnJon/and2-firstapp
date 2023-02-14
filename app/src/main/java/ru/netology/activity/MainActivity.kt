e39f28b15d264f8bfadbc31d3c932d5d0fa1cf20
SnJon <snjon93@gmail.lcom>
2023-02-15 01:08:37 +0900

    add editing activity & onPlay

--- app/src/main/java/ru/netology/activity/MainActivity.kt
+++ app/src/main/java/ru/netology/activity/MainActivity.kt
@@ -1,27 +1,42 @@
 package ru.netology.activity
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
 import androidx.activity.viewModels
 import ru.netology.R
 import ru.netology.adapter.OnInteractionListener
 import ru.netology.adapter.PostAdapter
 import ru.netology.databinding.ActivityMainBinding
 import ru.netology.dto.Post
-import ru.netology.util.hideKeyboard
 import ru.netology.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     private val viewModel: PostViewModel by viewModels()
     private val interaction = object : OnInteractionListener {
+
+        val editPostLauncher = registerForActivityResult(NewPostResultContract()) { result ->
+            result ?: return@registerForActivityResult
+            viewModel.changeContent(result)
+            viewModel.save()
+        }
+
         override fun onLike(post: Post) {
             viewModel.likeById(post.id)
         }
 
         override fun onShare(post: Post) {
             viewModel.shareById(post.id)
+            val intent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, post.content)
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(intent, getString(R.string.chooser_share_post))
+            startActivity(shareIntent)
+
         }
 
         override fun onRemove(post: Post) {
@@ -29,9 +44,18 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onEdit(post: Post) {
-            viewModel.edit(post)
-            activityMainBinding?.cancelEditingView?.cancelEditingView?.visibility = View.VISIBLE
 
+            editPostLauncher.launch(post.content)
+            viewModel.edit(post)
+        }
+
+        override fun onPlay(post: Post) {
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                data = Uri.parse(post.video)
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
         }
     }
 
@@ -43,35 +67,14 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         subscribe(binding)
 
-        binding.editingView.save.setOnClickListener {
-            with(binding.editingView.content) {
-                if (text.isNullOrBlank()) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        context.getString(R.string.error_empty_content),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
-                viewModel.changeContent(text.toString())
-                viewModel.save()
-
-                setText("")
-                clearFocus()
-                hideKeyboard()
-
-                binding.cancelEditingView.cancelEditingView.visibility = View.GONE
-            }
+        val newPostLauncher = registerForActivityResult(NewPostResultContract()) { result ->
+            result ?: return@registerForActivityResult
+            viewModel.changeContent(result)
+            viewModel.save()
         }
 
-        binding.cancelEditingView.cancel.setOnClickListener {
-            with(binding.editingView.content) {
-                setText("")
-                clearFocus()
-                hideKeyboard()
-                viewModel.save()
-                binding.cancelEditingView.cancelEditingView.visibility = View.GONE
-            }
+        binding.fab.setOnClickListener {
+            newPostLauncher.launch(null)
         }
     }
 
@@ -80,16 +83,6 @@ class MainActivity : AppCompatActivity() {
         binding.list.adapter = adapter
         viewModel.data.observe(this) { posts ->
             adapter.submitList(posts)
-        }
-
-        viewModel.edited.observe(this) { post ->
-            if (post.id == 0L) {
-                return@observe
-            }
-            with(binding.editingView.content) {
-                requestFocus()
-                setText(post.content)
-            }
         }
     }
 }
